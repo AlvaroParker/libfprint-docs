36a9ff96b81874ee2719df6dcb662bafe571f6bf
Parker <alvaroparker@users.noreply.github.com>
2023-10-25 17:13:27 +0000

    Automated docs

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libfprint_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"libfprint_rs/enum.FpFinger.html\" title=\"enum libfprint_rs::FpFinger\">FpFinger</a>"]]
+"libfprint_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"libfprint_rs/enum.FpFinger.html\" title=\"enum libfprint_rs::FpFinger\">FpFinger</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "libfprint_rs":["",[["device",[],["callback.rs","device.rs","device_sync.rs","enums.rs","mod.rs","user_data.rs"]]],["context.rs","finger.rs","image.rs","lib.rs","print.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
